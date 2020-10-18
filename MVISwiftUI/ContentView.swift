9975f7f9ad3ebedfd5c7a156a53db3d9a1fe2e2e
anonymous <anonymous>
2020-10-18 14:56:17 +0700

    Initial Commit

---
+++ MVISwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MVISwiftUI
+//
+//  Created by Endtry on 18/10/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVISwiftUI/MVISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVISwiftUIApp.swift
+//  MVISwiftUI
+//
+//  Created by Endtry on 18/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct MVISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
